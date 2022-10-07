30d71f3bcacca7891fd2f09072540f8aad5dc8d3
Narawit Muksuwan <6310611097@cn103>
2022-10-08 03:00:17 +0700

    solve problems

--- log_in/models.py
+++ log_in/models.py
@@ -2,17 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Subject(models.Model):
-    Id = models.CharField(max_length=40, primary_key=True)
+    Id = models.CharField(max_length=10, primary_key=True)
     Name = models.CharField(max_length=64)
     Credit = models.CharField(max_length=1)
     Term = models.CharField(max_length=1, default='1')
     Year = models.CharField(max_length=4, default='2022')
-    remainSeat = models.IntegerField(default=0)
+    remainSeat = models.IntegerField(default=10)
 
 class Student(models.Model):
     courses = models.ManyToManyField(Subject)
     users = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Course(models.Model):
-    users = models.ForeignKey(Student, on_delete=models.CASCADE)
-    course  = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    Id = models.CharField(max_length=10, primary_key=True)
+    Name = models.CharField(max_length=64)
+    Credit = models.CharField(max_length=1)
+    Term = models.CharField(max_length=1, default='1')
+    Year = models.CharField(max_length=4, default='2022')
