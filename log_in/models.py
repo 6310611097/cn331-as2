5db9d18896fcea5473dd5bc47d0b7420f9072078
Narawit Muksuwan <6310611097@cn103>
2022-10-08 22:52:46 +0700

    tests as4

--- log_in/models.py
+++ log_in/models.py
@@ -9,9 +9,18 @@ class Subject(models.Model):
     Year = models.CharField(max_length=4, default='2022')
     remainSeat = models.IntegerField(default=10)
 
+    def __str__(self):
+        return f"{ self.Id } { self.Name}"
+    
+    def is_seat_available(self):
+        return self.remainSeat < 0
+
 class Student(models.Model):
     courses = models.ManyToManyField(Subject)
     users = models.ForeignKey(User, on_delete=models.CASCADE)
+ 
+    def  __str__(self):
+        return f"{ self.users.first_name } { self.users.last_name}"
 
 class Course(models.Model):
     Id = models.CharField(max_length=10, primary_key=True)
--- log_in/tests.py
+++ log_in/tests.py
@@ -1,3 +1,50 @@
-from django.test import TestCase
+from re import template
+from django.urls import reverse
+from django.shortcuts import redirect
+from django.test import TestCase, Client
+from .models import Subject, Student
 
-# Create your tests here.
+class testCode(TestCase):
+    def setUp_Subject(self):
+        sub = Subject.objects.create(
+        ID = "CNXXX", 
+        Name = "XXXXXX", 
+        Credit = "3",
+        Year = "2066",
+        )
+        Subject.save()
+        self.assertEqual(str(sub), "CNXXX XXXXXX")
+
+    def setUp_Student(self):
+        user = Student.objects.create(
+            first_name = "a",
+            last_name = "pencil"
+        )
+        user.save()
+        self.assertEqual(str(user), "a pencil")
+
+    def test_reaminSeat_available(self):
+        sub=Subject.objects.first()
+        self.assertTrue(sub.is_seat_available())
+
+    def test_login_page(self):
+        cl = Client()
+        web =cl.get(redirect('/'))
+        self.assertEqual(web.status_code, 200)
+    
+    def test_index_page(self):
+        cl = Client()
+        web =cl.get(redirect('/home'))
+        self.assertEqual(web.status_code, 200)
+
+    def test_quota_page(self):
+        cl = Client()
+        web =cl.get(redirect('/result'))
+        self.assertEqual(web.status_code, 200)
+
+    def test_login(self):
+        self.client = Client()
+        web = self.client.get(redirect('/home'),{"username": "a", "password": "aaa11111"})
+        web = self.client.get(redirect('/home'))
+        self.assertEqual(web.status_code, 200)
+        
